04e1464031b326d9a65e4bc81e08ddb69f756918
Aliaksei Savanchuk <dntsaygoodbye@gmail.com>
2018-03-04 19:10:24 +0300

    Add the "Find props for an imported component" test
    The test triggers suggestions list for imported component and checks if
    vscode can provide proptypes in suggestions list.

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -6,16 +6,65 @@
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
 
+//import { activate } from '../src/extension';
+import * as vscode from 'vscode';
+import { completionItemsEquals, pathEquals } from './utils';
+import * as path from 'path';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 // import * as vscode from 'vscode';
 // import * as myExtension from '../extension';
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite('Extension Tests', () => {
-    // Defines a Mocha unit test
-    test('Something 1', () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+suite('Extension', () => {
+    test('Find props for an imported component', done => {
+        const workspace = vscode.workspace;
+        const cursorPositionForComponent = new vscode.Position(13, 35);
+        const proposal = [
+            new vscode.CompletionItem('boolProp', vscode.CompletionItemKind.Property),
+            new vscode.CompletionItem('funcProp', vscode.CompletionItemKind.Property),
+            new vscode.CompletionItem('objectProp', vscode.CompletionItemKind.Property)
+        ];
+        const workspaceFolders = workspace!.workspaceFolders;
+        assert.ok(workspaceFolders!.length > 0);
+        const workspaceFolder = workspaceFolders![0];
+        assert.ok(
+            pathEquals(workspaceFolder.uri.fsPath, path.join(__dirname, '../../testWorkspace'))
+        );
+        const indexJsxPath = path.join(workspaceFolder.uri.fsPath, './index.jsx');
+        const indexJsxUriPath = vscode.Uri.file(indexJsxPath);
+        workspace
+            .openTextDocument(indexJsxPath)
+            .then(vscode.window.showTextDocument, err => {
+                throw err;
+            })
+            .then(
+                document => {
+                    vscode.commands
+                        .executeCommand<vscode.CompletionList>(
+                            'vscode.executeCompletionItemProvider',
+                            indexJsxUriPath,
+                            cursorPositionForComponent
+                        )
+                        .then(
+                            (result: vscode.CompletionList | undefined) => {
+                                proposal.forEach(elem => {
+                                    const ifCompletionItemWasPresent = result!.items.find(
+                                        (item: vscode.CompletionItem, index: number) => {
+                                            return completionItemsEquals(elem, item);
+                                        }
+                                    );
+                                    assert.ok(ifCompletionItemWasPresent);
+                                });
+                                done();
+                            },
+                            err => {
+                                throw err;
+                            }
+                        );
+                },
+                err => {
+                    throw err;
+                }
+            );
     });
 });
