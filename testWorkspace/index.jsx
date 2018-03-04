12d5732e8985bae36e40d99dd79e2e10f0149d9a
Aliaksei Savanchuk <dntsaygoodbye@gmail.com>
2018-03-04 19:07:10 +0300

    Add test componentThis data is going to be used in integration tests.

---
+++ testWorkspace/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import './ComponentToImport';
+
+export default class MainComponent extends React.Component {
+    render() {
+        return (
+            <div className="classislav">
+                <h1>My name is {this.props.name}</h1>
+                <ul>
+                    <li>Turnstile</li>
+                    <li>Bane</li>
+                    <li>No warning</li>
+                </ul>
+                <ComponentToImport />
+            </div>
+        );
+    }
+}
