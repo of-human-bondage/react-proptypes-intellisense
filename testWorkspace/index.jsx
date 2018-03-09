0fa41bbd301c530ae4e561dafb53ed404d3ece0d
Pavel Hetsman <kv4sha@gmail.com>
2018-03-09 21:52:23 +0300

    Fix formatting issues

--- testWorkspace/index.jsx
+++ testWorkspace/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ComponentToImport from './ComponentToImport';
-import ComponentWithStaticPropTypes from './ComponentWithStaticPropTypes'
+import ComponentWithStaticPropTypes from './ComponentWithStaticPropTypes';
 
 export default class MainComponent extends React.Component {
     render() {
