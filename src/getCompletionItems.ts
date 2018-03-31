63784b3bea0c7a785cbec4dc894e23ce8bc7f88c
Pavel Hetsman <kv4sha@gmail.com>
2018-03-31 12:03:17 +0300

    Add documentation property in CompletionITem

--- src/getCompletionItems.ts
+++ src/getCompletionItems.ts
@@ -1,9 +1,22 @@
-import { TextDocument, CompletionItem, CompletionItemKind } from 'vscode';
+import { TextDocument, CompletionItem, CompletionItemKind, MarkdownString, commands } from 'vscode';
 
 import { ObjectExpression, ObjectProperty, Identifier } from 'babel-types';
 import babelTraverse, { Scope } from 'babel-traverse';
 
+import * as prettier from 'prettier';
+
 import { sourceLocationToRange } from './utils';
+
+const PRETTIER_OPTIONS: prettier.Options = {
+    tabWidth: 4,
+    semi: false
+};
+
+const getMarkdownString = (str: string): MarkdownString => {
+    const formattedStr = prettier.format(str, PRETTIER_OPTIONS);
+
+    return new MarkdownString().appendCodeblock(formattedStr);
+};
 
 const getCompletionItem = (
     objectProperty: ObjectProperty,
@@ -13,8 +26,9 @@ const getCompletionItem = (
 
     const completionItem = new CompletionItem(objectPropertyKey.name, CompletionItemKind.Field);
     completionItem.sortText = ''; // move on the top of the list
-    completionItem.detail = componentTextDocument.getText(
-        sourceLocationToRange(objectProperty.value.loc)
+    completionItem.detail = `(property) ${objectPropertyKey.name}`;
+    completionItem.documentation = getMarkdownString(
+        componentTextDocument.getText(sourceLocationToRange(objectProperty.value.loc))
     );
 
     return completionItem;
